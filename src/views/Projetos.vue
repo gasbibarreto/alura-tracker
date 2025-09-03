45bb54626d96ce39555a35b092d36cbf690d07e2
Gabriella Soares <gabriellakbsoares@gmail.com>
2025-09-03 06:05:58 -0300

    implementando a view de projetos

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,15 +1,64 @@
 <script lang="ts">
+import IProjeto from '@/interfaces/IProjeto'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'Projetos'
+  name: 'Projetos',
+  data( ) {
+    return {
+      nomeDoProjeto: "",
+      projetos: [ ] as IProjeto[]
+    }
+  },
+  methods: {
+    salvarProjeto() {
+      const projeto: IProjeto = {
+        nome: this.nomeDoProjeto,
+        id: new Date().toISOString( )
+      }
+      this.projetos.push(projeto)
+      this.nomeDoProjeto = ""
+    }
+  }
 })
 </script>
 
 <template>
-  <div class="projetos">
+  <section class="projetos">
+    <div class="title">
     <h1>Projetos</h1>
+    <form @submit.prevent="salvarProjeto">
+      <div class="field">
+        <label form="nomeDoProjeto" class="label">Nome do projeto</label>
+        <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjeto" />
+      </div>
+      <div class="field">
+      <button class="button" type="submit">Salvar</button>
+    </div>
+  </form>
+  <table class="table is-fullwidth">
+    <thead>
+      <tr>
+        <th>
+          ID
+        </th>
+        <th>
+         Nome
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="projeto in projetos" :key="projeto.id">
+        <td>{{ projeto.id }}</td>
+        <td>{{ projeto.nome }}</td>
+
+      </tr>
+    </tbody>
+  </table>
+    
   </div>
+  </section>
+  
 </template>
 
 <style scoped>
